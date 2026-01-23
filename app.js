659f49647eb28d41e589939c62c37864f87496ad
juan jose barrera <juanjoseb9805@gmail.com>
2026-01-23 01:13:06 -0500

    feat: login working, guard working, json sever working as well

---
+++ app.js
@@ -0,0 +1,45 @@
+const sendButton = document.querySelector("#sendBtn");
+const logout = document.querySelector("#logout");
+
+const getData = async function () {
+  const url = "http://localhost:4000/users";
+  const response = await fetch(url);
+  const data = await response.json();
+
+  return data;
+};
+
+sendButton.addEventListener("click", async function () {
+  const inputPassword = document.querySelector("#password");
+  const inputEmail = document.querySelector("#email");
+  const users = await getData();
+
+  const userFound = users.find(
+    (user) =>
+      inputEmail.value === user.email && inputPassword.value === user.password,
+  );
+
+  if (userFound) {
+    sessionStorage.setItem("isLogged", "true");
+    Swal.fire({
+      title: "Welcome!",
+      text: "You clicked the button!",
+      icon: "success",
+    });
+    setTimeout(() => {
+      window.location.href = "admin.html";
+    }, 2000);
+  } else {
+    Swal.fire({
+      title: "Error!",
+      text: "You clicked x the button!",
+      icon: "error",
+    });
+  }
+});
+
+logout.addEventListener("click", function () {
+  alert("s");
+  sessionStorage.removeItem("isLogged");
+  window.location = "login.html";
+});
---
+++ guard.js
@@ -0,0 +1,5 @@
+const logged = sessionStorage.getItem("isLogged");
+
+if (logged != "true") {
+  window.location.href = "login.html";
+}
